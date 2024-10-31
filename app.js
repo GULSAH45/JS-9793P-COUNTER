005ea9542b64e822e0087dafa12f1e845ed48108
GULSAH45 <178339362+GULSAH45@users.noreply.github.com>
2024-10-31 13:41:00 +0300

    done

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ var count = 0;
 function changeCount(num){
     count += num;
     document.getElementById("count").innerHTML = count;}
+
+const music = document.getElementById("music")
+
+function playmusic() {
+    if (music.paused) {
+
+        music.play()
+    }
+    else {
+        music.pause()
+
+    }
+
+
+}
